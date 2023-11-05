994dc72dd24ac3d418fb3bd97d59e387aa4c82e4
lokesh-cmi <lokesh.k@comakeit.com>
2023-11-05 20:35:29 +0530

    added styles to the authentication and chatpage

--- react-client/src/Pages/Authentication/Authentication.jsx
+++ react-client/src/Pages/Authentication/Authentication.jsx
@@ -1,102 +1,131 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import {backend} from "../../AxiosInstances/BackendInstance";
+import { backend } from "../../AxiosInstances/BackendInstance";
 
-// eslint-disable-next-line react/prop-types
-function Authentication({setAuthentication}) {
+function Authentication({ setAuthentication }) {
+  const [params] = useSearchParams();
+  const [mode, setMode] = useState(params.mode);
+  const navigate = useNavigate();
 
-    const [params] = useSearchParams();
-    const [mode,setMode] = useState(params.mode);
-    const navigate = useNavigate();
+  const userNameRef = useRef();
+  const passwordRef = useRef();
+  const emailRef = useRef();
 
-    const userNameRef = useRef();
-    const passwordRef = useRef();
-    const emailRef = useRef();
-
-
-    useEffect(()=>{
-        const mode = params.get('mode');
-        if(!(mode&&(mode==='login'||mode==='register'))){
-            navigate('/authentication?mode=login')
-        }
-        setMode(mode);
-    },[params]);
-
-    function authenticate(event){
-        event.preventDefault();
-
-        const userCredentials = {
-            userName:userNameRef.current.value,
-            userEmail:emailRef.current.value,
-            password:passwordRef.current.value
-        }
-
-        if(mode==='login'){
-            login(userCredentials)
-        }
-        else if(mode==='register') {
-            register(userCredentials);
-        }
-        
+  useEffect(() => {
+    const mode = params.get("mode");
+    if (!(mode && (mode === "login" || mode === "register"))) {
+      navigate("/authentication?mode=login");
     }
+    setMode(mode);
+  }, [params]);
 
-    function login(userCredentials){
-        console.log("trying to login the user");
-        backend.post('/authentication/login',userCredentials).then((response)=>{
-            const token = response.data.token;
-            if(token){
-                localStorage.setItem('token',token);
-                setAuthentication(true);
-                navigate('/chatbot');
-            }
-        }).catch((err)=>{
-            console.log("failed to login the user");
-            setAuthentication(false);
-            console.log(err);
-        })
+  function authenticate(event) {
+    event.preventDefault();
 
+    const userCredentials = {
+      userName: userNameRef.current.value,
+      userEmail: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    if (mode === "login") {
+      login(userCredentials);
+    } else if (mode === "register") {
+      register(userCredentials);
     }
+  }
 
-    function register(userCredentials){
-        console.log("trying to register the user");
-        backend.post('/authentication/register',userCredentials).then((response)=>{
-            const token = response.data.token;
-            if(token){
-                localStorage.setItem('token',token);
-                setAuthentication(true)
-                navigate('/chatbot')
-            }
-        }).catch((err)=>{
-            console.log("failed to regsiter the user");
-            setAuthentication(false);
-            console.log(err);
-        })
-    }
+  function login(userCredentials) {
+    console.log("trying to login the user");
+    backend
+      .post("/authentication/login", userCredentials)
+      .then((response) => {
+        const token = response.data.token;
+        if (token) {
+          localStorage.setItem("token", token);
+          setAuthentication(true);
+          navigate("/chatbot");
+        }
+      })
+      .catch((err) => {
+        console.log("failed to login the user");
+        setAuthentication(false);
+        console.log(err);
+      });
+  }
+
+  function register(userCredentials) {
+    console.log("trying to register the user");
+    backend
+      .post("/authentication/register", userCredentials)
+      .then((response) => {
+        const token = response.data.token;
+        if (token) {
+          localStorage.setItem("token", token);
+          setAuthentication(true);
+          navigate("/chatbot");
+        }
+      })
+      .catch((err) => {
+        console.log("failed to register the user");
+        setAuthentication(false);
+        console.log(err);
+      });
+  }
 
   return (
-    <section className="h-screen w-screen flex" >
-        <div className="authentication-left bg-black w-[50%] h-full ">
-            <h1></h1>
+    <section className="h-screen w-screen flex">
+      <div className="authentication-left bg-purple-500 w-1/2 h-full"></div>
+      <div className="authentication-right bg-gray-300 w-1/2 flex justify-center items-center h-full">
+        <div className="authenticate-form-container w-2/5">
+          <h1 className="text-purple-500 text-center text-3xl mb-7 capitalize font-medium">
+            {mode}
+          </h1>
+          <form onSubmit={authenticate} className="flex flex-col gap-3 authenticate-form">
+            <input
+              ref={userNameRef}
+              className="h-12 w-full rounded-lg pl-3 bg-white border-2 focus:outline-none focus:border-purple-500"
+              placeholder="Username"
+              type="text"
+            />
+            <input
+              ref={emailRef}
+              className="h-12 w-full rounded-lg pl-3 bg-white border-2 focus:outline-none focus:border-purple-500"
+              placeholder="Email"
+              type="email"
+            />
+            <input
+              ref={passwordRef}
+              className="h-12 w-full rounded-lg pl-3 bg-white border-2 focus:outline-none focus:border-purple-500"
+              placeholder="Password"
+              type="password"
+            />
+            <button className="w-full h-12 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition duration-300" type="submit">
+              {mode}
+            </button>
+          </form>
+          <p className="mt-5 text-gray-500">
+            {mode === "login" ? (
+              <span>
+                New user?{" "}
+                <Link to="/authentication?mode=register" className="text-purple-500">
+                  Register
+                </Link>
+              </span>
+            ) : (
+              <span>
+                Already have an account?{" "}
+                <Link to="/authentication?mode=login" className="text-purple-500">
+                  Login
+                </Link>
+              </span>
+            )}
+          </p>
         </div>
-        <div className="authentication-right bg-white w-[50%] flex justify-center items-center h-full">
-                <div className="authenticate-form-container w-[40%] ">
-                    <h1 className="text-black text-center text-[3rem] mb-7 capitalize font-medium " > {mode} </h1>
-                    <form onSubmit={authenticate} className="flex flex-col gap-2 authenticateform " >
-                        <input ref={userNameRef} className=" h-10 w-full rounded-lg pl-3 bg-white border-2 " placeholder="username" type="text" />
-                        <input ref={emailRef} className=" h-10 w-full rounded-lg pl-3 bg-white border-2" placeholder="email" type="email" />
-                        <input ref={passwordRef} className=" h-10 w-full rounded-lg pl-3 bg-white border-2" placeholder="password" type="password" />
-                        <button className="w-full h-10 bg-black text-white" type="submit" >  {mode} </button>
-                    </form>
-                    <span className="mt-5 block" >
-                        {
-                            mode==='login'? <p className="text-gray-500" >New user? <Link to='/authentication?mode=register' className="text-black" >Register</Link> </p> : <p className="text-gray-500">Already have an account ? <Link to='/authentication?mode=login' >Login</Link> </p>
-                        }
-                    </span>
-                </div>
-        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Authentication;
--- react-client/src/Pages/Chat/ChatPage.jsx
+++ react-client/src/Pages/Chat/ChatPage.jsx
@@ -1,181 +1,164 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-mixed-spaces-and-tabs */
 import { useEffect, useRef, useState } from "react";
 import io from 'socket.io-client';
 import { useNavigate } from "react-router-dom";
+
 function ChatPage() {
-
   const navigate = useNavigate();
-
   const inputRef = useRef(null);
   const chatNameRef = useRef(null);
   const roomNameRef = useRef(null);
-
   const token = localStorage.getItem('token');
+  const [socket] = useState(() => io('http://localhost:8080/chat', { auth: { token: token ? token : null } }));
+  const [messages, setMessages] = useState([]);
+  const [chatHistory, setChatHistory] = useState([]);
+  const [currentRoomId, setCurrentRoomId] = useState(null);
 
-  const [socket] = useState(()=>{
-	console.log("setting the socket ");
-
-	return io('http://localhost:8080/chat',{auth:{token:token?token:null}})
-  });
-  const [messages,setMessages] = useState([]);
-
-  const [chatHistory,setChatHistory] = useState([]); 
-
-  const [currentRoomId,setCurrentRoomId] = useState(null);
-
-  useEffect(()=>{
-
-    function connectToChatSession(){
+  useEffect(() => {
+    function connectToChatSession() {
       const token = localStorage.getItem('token');
-      if(!token){
+      if (!token) {
         navigate('/authentication?mode=login');
         return;
-	  }
-      socket.on('connect',()=>{});
+      }
+      socket.on('connect', () => { });
     }
-	console.log("connecting to the server .......");
-	connectToChatSession();
+    connectToChatSession();
+  }, []);
 
-    
-  },[]);
+  useEffect(() => {
+    function recieveMessage() {
+      socket.on('recieveMessage', (messages) => {
+        setMessages((previousState) => {
+          return [...previousState, ...messages];
+        });
+      });
+    }
+    recieveMessage();
+  }, []);
 
+  useEffect(() => {
+    function getRooms() {
+      socket.emit('getRoomsByUserName', (rooms, error) => {
+        if (error) {
+          alert("Cannot fetch previous chats");
+          return;
+        }
+        if (rooms) {
+          setChatHistory(rooms);
+        }
+      });
+    }
+    getRooms();
+  }, []);
 
-  useEffect(()=>{
-	function recieveMessage(){
-		socket.on('recieveMessage',(messages)=>{
-			console.log(messages);
-			setMessages((previousSate)=>{
-				return [...previousSate,...messages];
-			})
-		})
-	}
-
-	recieveMessage();
-  },[])
-
-  useEffect(()=>{
-	function getRooms(){
-		socket.emit('getRoomsByUserName',(rooms,error)=>{
-			if(error){
-				alert("cannot fetch previous chats");
-				return;
-			}
-			if(rooms){
-				setChatHistory(rooms);
-			}
-			
-		})
-	}
-
-	getRooms();
-  },[])
-
-  function handleSubmit(event){
-	event.preventDefault();
-	const message = inputRef.current.value;
-	if(message&&currentRoomId){
-		console.log("sending the message ",message);
-		socket.emit('sendMessage',message,currentRoomId,(responseData,error)=>{
-			console.log(responseData);
-			console.log(error);
-		});
-		inputRef.current.value = "";
-	}
+  function handleSubmit(event) {
+    event.preventDefault();
+    const message = inputRef.current.value;
+    if (message && currentRoomId) {
+      socket.emit('sendMessage', message, currentRoomId, (responseData, error) => {
+        console.log(responseData);
+        console.log(error);
+      });
+      inputRef.current.value = "";
+    }
   }
 
-  function joinRoom(roomId){
-	if(roomId){
-		socket.emit('joinRoom',roomId,(joinedRoom,error)=>{
-			if(error){
-				alert("cannot fetch chats");
-				return;
-			}
-			if(joinedRoom){
-				getMessagesOfRoom();
-				setChatHistory((previousSate)=>{
-					return [...previousSate,joinedRoom];
-				})
-			}
-		});
-	}
+  function joinRoom(roomId) {
+    if (roomId) {
+      socket.emit('joinRoom', roomId, (joinedRoom, error) => {
+        if (error) {
+          alert("Cannot fetch chats");
+          return;
+        }
+        if (joinedRoom) {
+          getMessagesOfRoom();
+          setChatHistory((previousState) => {
+            return [...previousState, joinedRoom];
+          });
+        }
+      });
+    }
   }
 
-  function getMessagesOfRoom(roomId){
-	socket.emit("getMessagesofRoom",roomId,(messages,error)=>{
-		if(error){
-			alert("cannot get messages of room ");
-			return;
-		}
-		setMessages(messages);
-	})
+  function getMessagesOfRoom(roomId) {
+    socket.emit("getMessagesofRoom", roomId, (messages, error) => {
+      if (error) {
+        alert("Cannot get messages of room");
+        return;
+      }
+      setMessages(messages);
+    });
   }
 
-  function createChatRoom(){
-	const roomName = chatNameRef.current.value;
-	socket.emit('createRoom',roomName,(response,_err)=>{
-		if(response){
-			setChatHistory((previousState)=>{
-				return [...previousState,response];
-			});
-		}
-		else{
-			console.warn("room cannot be created");
-		}
-	});
-	chatNameRef.current.value = "";
+  function createChatRoom() {
+    const roomName = chatNameRef.current.value;
+    socket.emit('createRoom', roomName, (response, _err) => {
+      if (response) {
+        setChatHistory((previousState) => {
+          return [...previousState, response];
+        });
+      } else {
+        console.warn("Room cannot be created");
+      }
+      chatNameRef.current.value = "";
+    })
   }
 
   return (
-    <section className="h-screen w-screen flex flex-[0.8] p-5 " >
-
-		<div className="chat_history h-full flex-[0.2] p-5 bg-black rounded-3xl ">
-			<input ref={chatNameRef} type="text" placeholder="Enter chatname" className="w-full h-9 mb-5 rounded-lg pl-5" />
-			<button onClick={createChatRoom} className="bg-white text-black w-full h-8 rounded-xl" >New chat</button>
-			<span className="block h-10" ></span>
-			<input ref={roomNameRef} type="text" placeholder="Enter chatname" className="w-full h-9 mb-5 rounded-lg pl-5" />
-			<button onClick={()=>{
-				const roomId = roomNameRef.current.value;
-				if(roomId){
-					joinRoom(roomId);
-					roomNameRef.current.value = "";
-				}
-					
-				else alert("enter a roomid");
-			}} className="bg-white text-black w-full h-8 rounded-xl" >Join chat</button>
-			<div className="chats mt-10 flex flex-col gap-4 ">
-			{
-				chatHistory.map((room,index)=>{
-					return <button key={index} onClick={()=>{		
-						setCurrentRoomId(room.roomId);
-						getMessagesOfRoom(room.roomId);
-					}} className="w-full pt-2 pb-2 rounded-md capitalize  bg-white" >{room.roomName}</button>
-				})
-			}
-			</div>
-		</div>
-
-		<div className="chat_window flex-[0.8] pl-5 pr-5">
-			<div className="chat_messages h-[90%] ">
-			{
-				messages.map((chat,index)=>{
-					return <div key={index}  >
-						<h1 style={{
-							textAlign:chat.userName === 'chatbot'? 'right':'left'		
-						}} > {chat.message} </h1>	
-					</div>
-				})
-			}
-			</div>
-			<form className="w-full flex justify-between " onSubmit={handleSubmit}>
-				<input className="h-10 flex-[0.8]  border border-black pl-5" ref={inputRef} type="text" placeholder="enter the message" />
-				<button className=" flex-[0.15] bg-black pl-5 pr-5 pt-3 pb-3 text-white rounded-lg " type="submit" >Send</button>
-			</form>
-		</div>
-		
-        
+    <section className="h-screen w-screen flex p-5">
+      <div className="chat_history h-full w-1/4 p-5 bg-purple-500 rounded-lg">
+        <div className="lavender-bg p-4 rounded-lg">
+          <input ref={chatNameRef} type="text" placeholder="Enter chat name" className="w-full h-9 mb-5 rounded-lg pl-3 bg-gray-200 text-black" />
+          <button onClick={createChatRoom} className="bg-purple-700 text-white w-full h-8 rounded-xl hover:bg-purple-800 transition-colors duration-300 ease-in-out">
+            New chat
+          </button>
+          <div className="border-t border-b border-purple-600 my-5"></div>
+          <input ref={roomNameRef} type="text" placeholder="Enter room ID" className="w-full h-9 mb-5 rounded-lg pl-3 bg-gray-200 text-black" />
+          <button onClick={() => {
+            const roomId = roomNameRef.current.value;
+            if (roomId) {
+              joinRoom(roomId);
+              roomNameRef.current.value = "";
+            } else {
+              alert("Enter a room ID");
+            }
+          }} className="bg-purple-700 text-white w-full h-8 rounded-xl hover-bg-purple-800 transition-colors duration-300 ease-in-out">
+            Join chat
+          </button>
+        </div>
+        <div className="chats mt-10 flex flex-col gap-4">
+          {chatHistory.map((room, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                setCurrentRoomId(room.roomId);
+                getMessagesOfRoom(room.roomId);
+              }}
+              className="w-full pt-2 pb-2 rounded-md bg-gray-200 hover-bg-gray-300 text-black hover-text-black transition-colors duration-300 ease-in-out">
+              {room.roomName}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="chat_window w-3/4 pl-5 pr-5 bg-gray-300 rounded-lg">
+        <div className="chat_messages h-5/6 overflow-y-auto">
+          {messages.map((chat, index) => (
+            <div key={index} className={`mb-2 ${chat.userName === 'chatbot' ? 'text-left' : 'text-right'}`}>
+              <div className={`rounded p-3 ${chat.userName === 'chatbot' ? 'bg-purple-100 text-purple-900' : 'bg-purple-300 text-purple-900'} border border-purple-300 inline-block`}>
+                {chat.message}
+              </div>
+            </div>
+          ))}
+        </div>
+        <form className="w-full flex justify-between" onSubmit={handleSubmit}>
+          <input className="h-10 w-3/4 border border-purple-300 rounded-l pl-3" ref={inputRef} type="text" placeholder="Enter the message" />
+          <button className="w-1/4 bg-purple-700 text-white rounded-r p-3 hover-bg-purple-800 transition-colors duration-300 ease-in-out">
+            Send
+          </button>
+        </form>
+      </div>
     </section>
-  )
+  );
 }
 
 export default ChatPage;
